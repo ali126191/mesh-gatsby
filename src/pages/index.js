8883b99d8bff2515f6d452f1160e4346ca8d13e0
shrivitthal <shrivitthalhindumandir@gmail.com>
2019-06-02 17:53:58 -0600

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -122,7 +122,7 @@ const IndexPage = () => (
                           <p className="contact__email">Mesfinkahsai@gmail.com</p>
                       </div>
                       <div className="map-responsive">
-                        <iframe title="Google Maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2508.1209382078573!2d-114.07985168394204!3d51.05085467956264!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x53716fe56a979d3b%3A0x54ee310aac7419fa!2s738+3+Ave+SW%2C+Calgary%2C+AB+T2P+0G7!5e0!3m2!1sen!2sca!4v1557673711628!5m2!1sen!2sca" frameBorder="0" style={{"border":"0", "width":"600", "height":"450"}} allowfullscreen></iframe>
+                        <iframe title="Google Maps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2500.2416750391963!2d-113.9980747492839!3d51.19619834113075!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x537160e36b7269cb%3A0x3089d4a755490e2f!2s260300+Writing+Creek+Cres%2C+Balzac%2C+AB+T0M+0E0!5e0!3m2!1sen!2sca!4v1559513976891!5m2!1sen!2sca" frameBorder="0" style={{"border":"0", "width":"600", "height":"450"}} allowfullscreen></iframe>
                       </div>
                   </div>
               </section>     
